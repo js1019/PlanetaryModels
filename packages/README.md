e22054897825e750178483837a90f157b9ba676a
Jia Shi <shijia1019@gmail.com>
2018-02-22 19:38:51 -0500

    readme

---
+++ packages/README.md
@@ -0,0 +1,14 @@
+Here are three packages that are used for model building
+
+if you need to compute the gravity field, please compile 
+stfmmlib3d-1.2
+
+for a linux machine, please do
+
+module load matlab
+
+make lib
+make mwrap
+make mex-matlab 
+
+to build what we need. 
---
+++ packages/stfmmlib3d-1.2/contrib/mwrap-0.33.3/src/mwrap.hh
@@ -0,0 +1,103 @@
+/* A Bison parser, made by GNU Bison 2.7.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_MWRAP_HH_INCLUDED
+# define YY_YY_MWRAP_HH_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     NON_C_LINE = 258,
+     NEW = 259,
+     TYPEDEF = 260,
+     CLASS = 261,
+     FORTRAN = 262,
+     ID = 263,
+     NUMBER = 264,
+     STRING = 265,
+     INPUT = 266,
+     OUTPUT = 267,
+     INOUT = 268
+   };
+#endif
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+/* Line 2058 of yacc.c  */
+#line 78 "mwrap.y"
+
+    char* string;
+    struct Func* func;
+    struct Var* var;
+    struct TypeQual* qual;
+    struct Expr* expr;
+    struct InheritsDecl* inherits;
+    char c;
+
+
+/* Line 2058 of yacc.c  */
+#line 81 "mwrap.hh"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void *YYPARSE_PARAM);
+#else
+int yyparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void);
+#else
+int yyparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
+
+#endif /* !YY_YY_MWRAP_HH_INCLUDED  */
