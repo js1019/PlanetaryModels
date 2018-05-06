deb947e542b92311c81e4d9b23454773954562cc
Jia Shi <shiji1019@gmail.com>
2018-05-06 16:44:43 -0400

    check sh

--- bin/compute_bridges.sh
+++ bin/compute_bridges.sh
@@ -2,15 +2,15 @@
 #SBATCH -J nm_model
 #SBATCH -p LM
 #SBATCH -C LM
-#SBATCH --mem 800GB
-#SBATCH --time=12:00:00
+#SBATCH --mem 400GB
+#SBATCH --time=4:00:00
 ##SBATCH -A TG-ASC160064
 #SBATCH --mail-user=shijia1019@gmail.com
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type=all
 
 echo "SLURM_NODELIST"=$SLURM_JOB_NODELIST
-cd /home/js116/Earth/modelbuilder
-module load matlab
-matlab -nodisplay -r "PREM_mesh; quit"
-#matlab -nodisplay -r "Gravity; quit"
+cd /home/js116/GitHub/PNMsG_models/modelbuilder
+module load matlab/R2018a
+#matlab -nodisplay -r "CONST_mesh; quit"
+matlab -nodisplay -r "Gravity; quit"
 
